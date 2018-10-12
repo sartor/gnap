a53a01748aa20d602cadf76092452dd18885cc13
sartor <sartorua@gmail.com>
2018-10-13 00:48:21 +0300

    post, tag, author pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,4 @@
 (function ($) {
  "use strict";
-    
-/*-----------------------------
-	Menu Stick
----------------------------------*/
-    $(window).on('scroll',function() {
-        if ($(this).scrollTop() > 1){  
-            $('.sticker').addClass("stick");
-        }   
-        else{
-            $('.sticker').removeClass("stick");
-        }
-    });
-    
-/*----------------------------
-    Toogle Search
------------------------------- */
-    // Handle click on toggle search button
-    $('.header-search').on('click', function() {
-        $('.search').toggleClass('open');
-        return false;
-    });
 
 })(jQuery); 
